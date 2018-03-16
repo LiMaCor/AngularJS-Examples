6d8da2d2836fd1db76de9475fc7942fd49f2e495
PixelZer0 <pixel_zero@hotmail.com>
2018-03-16 19:10:58 +0100

    Added contacts routes with params

--- chapter-4/assets/js/controller.js
+++ chapter-4/assets/js/controller.js
@@ -12,7 +12,7 @@
  * siguiente:
  * 
  * - angular.module("app", []) => La función 'module' establece un módulo
- *   que tendrá como nombre el primer parámetro que le pasemos, en este caso, app.
+ *   que tendrá como nombre el primer parámetro que le pasemos, en este caso, "app".
  *   Los corchetes indican un array en el que se pueden especificar dependencias
  *   que se pueden inyectar en el módulo, por ejemplo, 'ngRoute'.
  * 
@@ -21,7 +21,7 @@
  *   gestionar el funcionamiento de nuestra aplicación.
  * 
  * Por defecto, cuando indicamos la directiva 'ng-app' sin pasar ningún parámetro,
- * AngularJS establece un módulo que trae por defecto, permitiendonos ver fucionalidades
+ * AngularJS establece un módulo que trae por defecto, permitiendonos ver las fucionalidades
  * básicas del core. Si quisieramos crear un controlador, tendríamos que crear un módulo
  * y pasarlo como parámetro a la directiva 'ng-app'.
  * 
@@ -33,9 +33,21 @@
  * Si especificamos un controlador, tendremos que definirlo en nuestro módulo.
  * Con el método 'otherwise' estableceremos la ruta a seguir por nuestra aplicación
  * cuando esta no sepa que vista mostrar al usuario.
+ * 
+ * ========================================================================================
+ * 
+ * Inyectando el servicio '$locationProvider' podremos establecer un prefijo a nuestras rutas,
+ * por ejemplo:
+ * 
+ *          https://localhost:8000/#/add-contact
+ * 
+ * En la ruta proporcionada arriba, podemos observar que hay un hashtag '#' que precede a
+ * nuestra ruta. Este prefijo podemos cambiarlo si utilizamos una aplicación estructurada
+ * en perfiles, por ejemplo. En nuestro caso hemos decidido eliminarlo con el método
+ * 'html5Mode'.
  */
 
-angular.module("app", ['ngRoute']).config(function($routeProvider){
+angular.module("app", ['ngRoute']).config(function($routeProvider, $locationProvider) {
     $routeProvider.when('/', {
         controller: 'indexCtrl',
         templateUrl: 'assets/partials/index.html'
@@ -44,8 +56,18 @@ angular.module("app", ['ngRoute']).config(function($routeProvider){
         controller: 'addCtrl',
         templateUrl: 'assets/partials/add.html'
     })
-}).controller("indexCtrl", function($scope){
+    .when('/contact/:id', {
+        controller: 'contactCtrl',
+        templateUrl: 'assets/partials/contact.html'
+    })
+    .otherwise({
+        redirectTo: '/'
+    });
+    $locationProvider.html5Mode(true);
+}).controller("indexCtrl", function($scope) {
 
-}).controller("addCtrl", function($scope){
+}).controller("addCtrl", function($scope) {
 
-});
+}).controller("contactCtrl", function($scope, $routeParams) {
+
+})
